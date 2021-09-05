c76b4edbab60888571d86c9ebc9ffd9e05bb9b74
izuna385 <izuna385@gmail.com>
2021-09-05 13:50:37 +0900

    remove copyright

--- frontend/react_app/src/components/Login.js
+++ frontend/react_app/src/components/Login.js
@@ -12,22 +12,10 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import { useHistory, useLocation } from "react-router-dom";
 
 import { connect } from 'react-redux';
 import * as actions from '../store/authActions';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -117,9 +105,7 @@ function Login(props) {
 
         </form>
       </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
+
     </Container>
   );
 }
